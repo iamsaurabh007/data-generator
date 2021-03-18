b4afa0d390c3ed04b9ff47a0b35824b406c70ef9
dddaga <63771308+dddaga@users.noreply.github.com>
2021-03-18 20:13:47 +0530

    Update run.py

--- run.py
+++ run.py
@@ -51,7 +51,7 @@ if __name__ == '__main__':
             break
         
         
-    print('Image Generation finished for {}'.format(CORPUS_PATH))
+    print('Image Generation finished at {}'.format(OUTPATH))
     
 
 
