d0f365f4370862304f65c09b5c17c3fb3949d2f8
iamsaurabh007 <saurabh.yadav1947@gmail.com>
2021-03-30 13:00:34 +0530

    _

--- code.py
+++ code.py
@@ -22,10 +22,10 @@ def create_image(background,font,symbol,font_size,col,path):
     font_pil = ImageFont.truetype(font, font_size)
     ascent, descent = font_pil.getmetrics()  
     try:
-	a=font_pil.getmask(symbol).getbbox()[2]
+        a=font_pil.getmask(symbol).getbbox()[2]
     except TypeError:
- 	print("TYPEERROR handled due to",symbol)
-	a=font_pil.getmask('-').getbbox()[2]   
+        print("TYPEERROR handled due to",symbol)
+        a=font_pil.getmask('-').getbbox()[2]   
     text_width = a
     text_height = ascent+descent
     text_width+=int(0.10*(text_width))
